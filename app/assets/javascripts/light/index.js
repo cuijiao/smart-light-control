29faa2b963b8af638390f85a2f9f77a0503083f0
jiao.cui <jiao.cui@rea-group.com>
2016-11-04 09:35:40 +0800

    add total control

---
+++ app/assets/javascripts/light/index.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('#south-side-toggle').change(function () {
+        var data = {"light_num": "south", "status": "off"};
+
+        if (!$(this).parent().hasClass("off")) {
+            data = {"light_num": "south", "status": "on"}
+        }
+
+        $.post("http://localhost:3000/smart_light_control/publish", {"light_num": data}, function () {
+         console.log(data);
+        });
+    });
+
+    $('#middle-side-toggle').change(function () {
+        $.post("http://localhost:3000/smart_light_control/publish", {"light_num": "middle"}, function () {
+            alert("success")
+        });
+    });
+
+    $('#north-side-toggle').change(function () {
+        $.post("http://localhost:3000/smart_light_control/publish", {"light_num": "north"}, function () {
+            alert("success")
+        });
+    });
+})
--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -10,7 +10,7 @@ class LightController < ApplicationController
     MQTT::Client.connect(conn_opts) do |c|
       c.publish('ruby', "Hello World #{params['light_num']}")
     end
-    redirect_to root_path
+    render :nothing=>true
   end
 
   private
