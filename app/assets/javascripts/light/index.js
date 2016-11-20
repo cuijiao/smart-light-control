1478347f23385e0524569f0e132b3a5734f4b75a
jiao.cui <jiao.cui@rea-group.com>
2016-11-20 23:40:19 +0800

    fix toggle button click event

--- app/assets/javascripts/light/index.js
+++ app/assets/javascripts/light/index.js
@@ -1,10 +1,2 @@
 $(function () {
-    $('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function (event, state) {
-        var status = state == true ? "on" : "off";
-        var data = {"section": this.id, "status": status};
-
-        $.post(SMARTCONTROL.domain + "/smart_light_control/publish", data, function () {
-            console.log(data);
-        });
-    });
 });
