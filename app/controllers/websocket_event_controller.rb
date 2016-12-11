6932358e0c5f7280dbd2cb9a3747aad1df303af9
jiao.cui <jiao.cui@rea-group.com>
2016-12-11 10:16:07 +0800

    show pending icon only when receive delay_switch_off event

--- app/controllers/websocket_event_controller.rb
+++ app/controllers/websocket_event_controller.rb
@@ -4,47 +4,40 @@ class WebsocketEventController < WebsocketRails::BaseController
   end
 
   def switch_on
-    p 'switch on'
-    switch_light data[:light_id], 'on'
-    broadcast_message :switch_on_success, data, :namespace => :light
+    switch_light data[:light_id], 'on', :switch_on_success
   end
 
   def switch_off
-    p 'switch off'
-    switch_light data[:light_id], 'off'
-    broadcast_message :switch_off_success, data, :namespace => :light
+    switch_light data[:light_id], 'off', :switch_off_success
   end
 
   def section_switch_on
     lights = Light.where('section = ?', data[:section])
     lights.each do |light|
-      switch_light light.light_id, 'on'
-      broadcast_message :switch_on_success, {'light_id' => light.light_id }, :namespace => :light
+      switch_light light.light_id, 'on', :switch_on_success
     end
   end
 
   def section_switch_off
     lights = Light.where('section = ?', data[:section])
     lights.each do |light|
-      switch_light light.light_id, 'off'
-      broadcast_message :switch_off_success, {'light_id' => light.light_id }, :namespace => :light
+      switch_light light.light_id, 'off', :switch_off_success
     end
   end
 
   private
 
-  def switch_light light_id, status
-    lights = Light.where('light_id = ?', light_id)
+  def switch_light light_id, status, callback
+    light = Light.where('light_id = ?', light_id)[0]
 
-    lights.each do |light|
-      if need_delay? && status == 'off'
-        light.switch_off
-        broadcast_message :switch_off_success, {'light_id' => light.light_id }, :namespace => :light
-        light.delay(run_at: 5.seconds.from_now).switch_on
-        light.delay(run_at: 60.seconds.from_now).switch_off
-      else
-        status == 'on' ? light.switch_on : light.switch_off
-      end
+    if need_delay? && status == 'off'
+      light.switch_off
+      light.delay(run_at: 3.seconds.from_now).switch_on
+      broadcast_message :delay_switch_off, {'light_id' => light_id}, :namespace => :light
+      light.delay(run_at: 60.seconds.from_now).switch_off
+    else
+      status == 'on' ? light.switch_on : light.switch_off
+      broadcast_message callback, {'light_id' => light_id}, :namespace => :light
     end
   end
 
